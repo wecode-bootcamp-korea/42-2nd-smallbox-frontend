3622f29f0a48762f8c4515f1a3c574e3adc802aa
laralina <laralina@laralinaui-MacBook-Pro.local>
2023-02-27 16:23:15 +0900

    ADD : nav 추가

--- src/Router.js
+++ src/Router.js
@@ -4,8 +4,7 @@ import Nav from './components/Nav/Nav';
 import Footer from './components/Footer/Footer';
 import MovieList from './pages/MovieList/MovieList';
 import MovieDetail from './pages/MovieDetail/MovieDetail';
-import Reservation from './pages/Reservation/Reservation';
-import Login from './pages/Login/Login';
+import Booking from './pages/Booking/Booking';
 import Main from './pages/Main/Main';
 
 const Router = () => {
@@ -14,8 +13,7 @@ const Router = () => {
       <Nav />
       <Routes>
         <Route path="/" element={<Main />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/reservation" element={<Reservation />} />
+        <Route path="/booking" element={<Booking />} />
         <Route path="/movieList" element={<MovieList />} />
         <Route path="/movieDetail" element={<MovieDetail />} />
       </Routes>
