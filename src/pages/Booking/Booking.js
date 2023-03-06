5638598f0435bd2903499f0ff832cb1a3b140cc8
laralina <laralina@laralinaui-MacBook-Pro.local>
2023-03-06 21:32:31 +0900

    ADD : 예약_날짜 컴포넌트 추가

--- src/pages/Booking/Booking.js
+++ src/pages/Booking/Booking.js
@@ -2,12 +2,14 @@ import React from 'react';
 import styled from 'styled-components';
 import BookingTime from './BookingTime';
 import BookingResult from './BookingResult';
+import DateComponent from './DateComponent';
 
 function Booking() {
   return (
     <BookingContainer>
       <BookingTime />
       <BookingResult />
+      <DateComponent />
     </BookingContainer>
   );
 }
